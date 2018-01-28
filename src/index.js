f65fa1b292902bc8b20ef97658e2b2bd52d64e61
Kenny <kennymax18@gmail.com>
2018-01-27 20:20:48 -0500

    Made some fixes and removed some elements.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ let css = require('./sass/style.scss');
 let videoCtrl = require('./scripts/footer_ctrl.js');
 let slider = require('./scripts/slides.js');
 let images = require('./scripts/card_images.js');
+
+let down = document.querySelector('.down');
+let dest = document.querySelector('.slides').offsetTop;
+
+down.onclick = function(){
+   scrollDown();
+}
+
+scrollDown = () => {
+    if(document.documentElement.scrollTop < dest){
+        document.documentElement.scrollTop+=30; 
+        requestAnimationFrame(scrollDown);
+    }
+};
--- src/scripts/card_images.js
+++ src/scripts/card_images.js
@@ -1,7 +1,7 @@
 let cards = document.getElementsByClassName('card_footer');
 let images = [
     'https://fthmb.tqn.com/jbF_Mehw1A8HlCnAkmrmiVDhBUA=/400x250/filters:no_upscale()/savasanajpg-5705610a5f8e9c32618cdb90',
-    'https://beginneryogi.com/wp-content/uploads/2014/10/Dollarphotoclub_553405361-e1442816645412.jpg',
+    'http://beherenowyoga108.com/wp-content/uploads/2014/03/108-Words-Cloud.jpg',
     'https://images.r.cruisecritic.com/features/2016/06/bottoms-up-hero.jpg',
     'https://www.success.com/sites/default/files/main/blogposts/waystotapintoyourcreativeself.jpg',
     'http://pioneerhumanservices.org/sites/default/files/facts-and-myths.jpg',
